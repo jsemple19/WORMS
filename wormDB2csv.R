b12de2fb37137dae307ca888a323d64a7e9710e1
jsemple19 <semple.j@gmail.com>
2017-06-27 15:45:25 +0200

    first commit

---
+++ wormDB2csv.R
@@ -0,0 +1,31 @@
+#another package:
+#library(xlsx)
+
+
+
+#setwd("~/Documents/MeisterLab")
+setwd("/Volumes/izb_groups/Meister/Stock/WORMS")
+
+xlsxFile<-"WormStrainFrozenStocks_testing.xlsx"
+
+# note readxl is much faster but gdata maintains format of columns with date much better, readxl
+# converts dates to number.
+
+library(gdata)
+mySheets<-sheetNames(xlsxFile)
+for (i in 2:8) {
+    df = read.xls (xlsxFile, sheet = i, header = TRUE,skip=1)
+    write.csv(df,file=paste0("csv/",mySheets[i],".csv"))
+ }
+
+
+
+# library(readxl)
+# mySheets<-excel_sheets(xlsxFile)
+#
+# for (i in 2:8) {
+#    df = read_excel(xlsxFile, sheet = 4, skip=1, col_names=TRUE)
+#    write.csv(df,file=paste0(mySheets[i],".csv"))
+# }
+
+
